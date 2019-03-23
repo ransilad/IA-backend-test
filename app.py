238d04d709c5ff593f9c61a89377cfc6651de84e
Abel Rangel <ransilad@gmail.com>
2019-03-23 13:16:11 -0300

    Adding Cors and fix indentations

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import boto3
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 dynamodb = boto3.resource(
@@ -15,25 +17,27 @@ dynamodb = boto3.resource(
 
 class Subscriptions(Resource):
 
-  def post(self):
+    def post(self):
 
-    table = dynamodb.Table('Subscriptions')
-    reqData = request.get_json()
-    successResponse = True
+        table = dynamodb.Table('Subscriptions')
+        reqData = request.get_json()
+        print(reqData)
+        successResponse = True
 
-    try:
-        table.put_item(
-            Item={
-                'email': reqData['email'],
-                'name': reqData['name'],
-                'phone': reqData['phone'],
-                'dni': reqData['dni']
-            }
-        )
-    except:
-        successResponse = False
+        try:
+            table.put_item(
+                Item={
+                    'email': reqData['email'],
+                    'name': reqData['name'],
+                    'phone': reqData['phone'],
+                    'dni': reqData['dni']
+                }
+            )
+        except Exception as e:
+            print(str(e))
+            successResponse = False
 
-    return { "success": successResponse }
+        return { "success": successResponse }
 
 api.add_resource(Subscriptions, '/landing/subscriptions')
 
